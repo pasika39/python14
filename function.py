c2393dd3195b5fce920065357c182277850fa3c5
Pasika Moonsri <50244@cru.ac.th>
2022-07-26 14:43:34 +0700

    function

---
+++ function.py
@@ -0,0 +1,11 @@
+#สร้างฟังก์ชั่น
+def hello():
+    print("Hello world")
+    print("Pasika")
+hello()
+#ส่งค่าผ่านparameter+return
+def sum(x,y):
+    x+y
+    return "hello"
+
+print(sum(5,8))
---
+++ work-function.py
@@ -0,0 +1,16 @@
+#สร้าางฟังก์ชั่น
+def square(width,height):
+    ans = width*height
+    print(ans)
+square(4,4)
+#คำสั่งหาพื้นที่สี่เหลี่ยม
+def triangle(base,height):
+    ans = 1/2*base*height
+    print(ans)
+triangle(5,12)
+#คำสั้งหาพื้นที่ววงกลม
+def circle(radius):
+    ans = 3.14*(radius*radius)
+    print(ans)
+circle(7)
+#ชื่อพศิกา มุลศรี เลขที่39 ม.6/14
